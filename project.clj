8e796662ad0546563ab97b6d1c9b820564aef575
Cam Saul <cammsaul@gmail.com>
2019-06-14 18:18:30 -0700

    Bump deps; 1.0.0 release

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject metabase/honeysql-util "1.0.0-SNAPSHOT"
+(defproject metabase/honeysql-util "1.0.0"
   :description "Helpful utility functions for HoneySQL."
   :url "https://github.com/metabase/honeysql-util"
   :min-lein-version "2.5.0"
@@ -19,7 +19,7 @@
   :dependencies
   [[honeysql "0.9.4" :exclusions [org.clojure/clojurescript]]
    [metabase/common "1.0.1"]
-   [prismatic/schema "1.1.9"]]
+   [prismatic/schema "1.1.11"]]
 
   :profiles
   {:dev
